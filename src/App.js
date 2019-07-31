8b2567e89110aa69f6c8af491a52dbe3f267c30e
Mauro Leggieri <mxmauro@mauroleggieri.com>
2019-07-31 17:19:28 -0300

    Applied ADPU formating

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import './App.css';
 import LedgerUSB from './LedgerUSB';
+import algosdk_address from 'algosdk/src/encoding/address';
 
 export default class App extends React.Component {
 	constructor (props) {
 		super(props);
 
+		this.ledgerUSB = new LedgerUSB();
+
+		this.state = {
+			address: '',
+			signedTx: '',
+			error: ''
+		}
+
 		this.allowUsbAccess = this.allowUsbAccess.bind(this);
 		this.getAddress = this.getAddress.bind(this);
-		this.ledgerUSB = new LedgerUSB();
-		
-		this.state = {
-			getAddress: {
-				address: '',
-				error: ''
-			}
-		}
+		this.signSampleTx = this.signSampleTx.bind(this);
 	}
 
 	async allowUsbAccess(e) {
@@ -23,24 +25,58 @@ export default class App extends React.Component {
 	}
 
 	async getAddress(e) {
+		e.preventDefault();
+
 		try {
 			let address = await this.ledgerUSB.getAddress();
 			this.setState({
-				getAddress: {
-					address,
-					error: ''
-				}
+				address,
+				signedTx: '',
+				error: ''
 			});
 		}
 		catch (err) {
 			this.setState({
-				getAddress: {
-					address: '',
-					error: err.stack ? err.stack.toString() : err.toString()
-				}
+				address: '',
+				signedTx: '',
+				error: err.stack ? err.stack.toString() : err.toString()
 			});
 		}
 	}
+
+	async signSampleTx(e) {
+		e.preventDefault();
+
+		try {
+			let tx = await this.ledgerUSB.signTransaction({
+				txn: {
+					type: 'pay',
+					snd: algosdk_address.decode('QQ2Q4MDDM2F7SMWWQRLOOPFL4ZKLUXM2MW2VLYXX4VHRB6D3R7M65USKHQ').publicKey,
+					fee: 1000,
+					fv: 3150,
+					lv: 3250,
+					rcv: algosdk_address.decode('MN3CIWJHPDMLYZ7RU2ZUBPJUE7SUIQYNI4DBGDC2XUZZEJ566LNIUBM2XI').publicKey,
+					amt: 10000,
+					note: Buffer.alloc(0),
+					gen: 'mainnet-v1.0',
+					gh: Buffer.from('wGHE2Pwdvd7S12BL5FaOP20EGYesN73ktiC1qzkkit8=', 'base64')
+				}
+			});
+			this.setState({
+				address: '',
+				signedTx: JSON.stringify(tx),
+				error: ''
+			});
+		}
+		catch (err) {
+			this.setState({
+				address: '',
+				signedTx: '',
+				error: err.stack ? err.stack.toString() : err.toString()
+			});
+		}
+	}
+
 
 	render() {
 		const isSupported = LedgerUSB.isSupported();
@@ -59,18 +95,34 @@ export default class App extends React.Component {
 					{isSupported ? (
 						<div style={{ width: '90%' }}>
 							<p>WebUSB is supported</p>
-							<p><button onClick={this.getAddress}>Get Address</button></p>
+							<p>
+								<button onClick={this.getAddress}>Get Address</button>
+								&nbsp;&nbsp;&nbsp;
+								<button onClick={this.signSampleTx}>Sign sample transaction</button>
+							</p>
 							<hr />
 							{
 								(
-									this.state.getAddress.address.length > 0 &&
-									<p>Address is: {this.state.getAddress.address}</p>
+									this.state.address.length > 0 &&
+									<p>Address is: {this.state.address}</p>
 								)
 							}
 							{
 								(
-									this.state.getAddress.error.length > 0 &&
-									<p>Error: <pre style={preBlockCSS}> {this.state.getAddress.error}</pre></p>
+									this.state.signedTx.length > 0 &&
+									<div>
+										Signed TX:<br />
+										<pre style={preBlockCSS}>{this.state.signedTx}</pre>
+									</div>
+								)
+							}
+							{
+								(
+									this.state.error.length > 0 &&
+									<div>
+										Error:<br />
+										<pre style={preBlockCSS}>{this.state.error}</pre>
+									</div>
 								)
 							}
 						</div>
--- src/LedgerUSB.js
+++ src/LedgerUSB.js
@@ -1,19 +1,16 @@
 import TransportWebUSB from '@ledgerhq/hw-transport-webusb';
 import TransportWebAuthn from "@ledgerhq/hw-transport-webauthn";
 import TransportU2F from "@ledgerhq/hw-transport-u2f";
-//import { Observable } from "rxjs";
-//import { map } from "rxjs/operators";
-//import { registerTransportModule } from "@ledgerhq/live-common/lib/hw";
 import algosdk_address from 'algosdk/src/encoding/address';
 import Queue from 'promise-queue';
-//import BN from 'bn.js';
+import BN from 'bn.js';
 
 //------------------------------------------------------------------------------
 
 const CLA = 0x80;
 const INS_GET_PUBLIC_KEY = 0x03;
-//const INS_SIGN_PAYMENT_V2 = 0x04;
-//const INS_SIGN_KEYREG_V2 = 0x05;
+const INS_SIGN_PAYMENT_V2 = 0x04;
+const INS_SIGN_KEYREG_V2 = 0x05;
 
 const STATUS_OK = 0x90;
 const STATUS_APP_NOT_LOADED = 0x6E;
@@ -38,8 +35,76 @@ export default class LedgerUSB {
 
 	async getAddress() {
 		await openLedger(this);
-		const publicKey = await queueCommand(this, Buffer.from([ CLA, INS_GET_PUBLIC_KEY ]));
-		return algosdk_address.encode(publicKey);
+		try {
+			const publicKey = await queueCommand(this, INS_GET_PUBLIC_KEY);
+			return algosdk_address.encode(publicKey);
+		}
+		catch (err) {
+			this.transport = null;
+			throw err;
+		}
+	}
+
+	async signTransaction(tx) {
+		if (!(isObject(tx) && isObject(tx.txn))) {
+			throw new Error("Invalid transaction");
+		}
+
+		let ins;
+		let data = [];
+
+		if (tx.txn.type === 'pay') {
+			ins = INS_SIGN_PAYMENT_V2;
+		}
+		else if (tx.txn.type === 'keyreg') {
+			ins = INS_SIGN_KEYREG_V2;
+		}
+		else {
+			throw new Error("Unknown transaction type " + tx.txn.type);
+		}
+		//sender
+		data.push(Buffer.from(tx.txn.snd));
+		//fee
+		data.push(uint64ToBuffer(tx.txn.fee));
+		//first round
+		data.push(uint64ToBuffer(tx.txn.fv));
+		//last round
+		data.push(uint64ToBuffer(tx.txn.lv));
+		//genesis id
+		data.push(padBuffer(tx.txn.gen, 32));
+		//genesis hash
+		data.push(Buffer.from(tx.txn.gh));
+		//if payment transaction
+		if (tx.txn.type === 'pay') {
+			//receiver
+			data.push(Buffer.from(tx.txn.rcv));
+			//amount
+			data.push(uint64ToBuffer(tx.txn.amt));
+			//close address
+			if (typeof tx.txn.close != "undefined" && tx.txn.close) {
+				data.push(Buffer.from(tx.txn.close));
+			}
+			else {
+				data.push(Buffer.alloc(32, 0));
+			}
+		}
+		/*
+		//if a keyreg transaction
+		else if (tx.txn.type === 'keyreg') {
+			data.push(padBuffer(txn["votekey"], 32)); //votepk
+			data.push(padBuffer(txn["selkey"], 32));  //vrfpk
+		}
+		*/
+
+		await openLedger(this);
+		try {
+			const signature = await queueCommand(this, ins, data);
+			return signature;
+		}
+		catch (err) {
+			this.transport = null;
+			throw err;
+		}
 	}
 }
 
@@ -56,14 +121,24 @@ async function openLedger(_this) {
 				_this.transport.setScrambleKey('algo');
 			}
 		}
-		if (TransportU2F.isSupported) {
-			_this.transport = await TransportU2F.create(); //this will create the first available device connected
-			if (_this.transport) {
-				_this.transport.setScrambleKey('algo');
+		if ((!_this.transport) && TransportU2F.isSupported) {
+			try {
+				_this.transport = await TransportU2F.create(); //this will create the first available device connected
+				if (_this.transport) {
+					_this.transport.setScrambleKey('algo');
+				}
+			}
+			catch (err) {
+				_this.transport = null;
 			}
 		}
 		if ((!_this.transport) && TransportWebUSB.isSupported) {
-			_this.transport = await TransportWebUSB.create(); //this will create the first available device connected
+			try {
+				_this.transport = await TransportWebUSB.create(); //this will create the first available device connected
+			}
+			catch (err) {
+				_this.transport = null;
+			}
 		}
 		if (!_this.transport) {
 			throw new Error("Device not found");
@@ -71,12 +146,42 @@ async function openLedger(_this) {
 	}
 }
 
-function queueCommand(_this, buffer) {
+function queueCommand(_this, ins, data) {
+
+	let command = [];
+	
+	command.push(Buffer.from([ CLA, ins, 0, 0 ]));
+
+	if (!data) {
+		data = Buffer.alloc(0);
+	}
+	else if (Array.isArray(data)) {
+		data = Buffer.concat(data);
+	}
+	if (data.byteLength === 0) {
+		command.push(Buffer.from([ 0 ]));
+	}
+	else if (data.byteLength <= 255) {
+		command.push(Buffer.from([ data.byteLength ]));
+		command.push(data);
+	}
+	/*
+	//NOT SUPPORTED YET
+	else if (data.byteLength <= 65535) {
+		command.push(Buffer.from([ 0, data.byteLength & 255, data.byteLength / 8 ]));
+		command.push(data);
+	}
+	*/
+	else {
+		throw new Error("Data too long");
+	}
+	command = Buffer.concat(command);
+
 	return new Promise((resolve, reject) => {
 		let _that = _this;
 
 		_this.commandQueue.add(function () {
-			return sendToLedger(_that.transport, buffer);
+			return sendToLedger(_that.transport, command);
 		}).then((result) => {
 			resolve(result);
 		}).catch((err) => {
@@ -106,7 +211,6 @@ async function sendToLedger(transport, buffer) {
 	return data.slice(0, data.length - 2);
 }
 
-/*
 function padBuffer(str, size) {
 	let ret = Buffer.alloc(size, 0);
 	if (str) {
@@ -121,4 +225,7 @@ function padBuffer(str, size) {
 function uint64ToBuffer(value) {
 	return (new BN(value)).toArrayLike(Buffer, 'le', 8);
 }
-*/
+
+function isObject(obj) {
+	return (typeof obj === 'object' && (!Array.isArray(obj)));
+}
